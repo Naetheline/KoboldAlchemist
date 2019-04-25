4f2c10dc59bcd55b2b7f2f7a5f92c3128d4a2a98
Naethaline <nathalie.megevand@gmail.com>
2019-04-25 13:08:04 +0200

    Clamp player position in background

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public const int LIMIT = 26;
     public const float ACCELERATION_MOD = 0.5f;
     public const float ACCELERATION_THRESHOLD = 0.1f;
     void Update()
@@ -16,5 +17,7 @@ public class PlayerControl : MonoBehaviour
         }
 
         transform.Translate(acceleration * ACCELERATION_MOD, 0, 0);
+        Vector3 clampedPosition = new Vector3( Mathf.Clamp(transform.position.x, -LIMIT, LIMIT), transform.position.y, transform.position.z);
+        transform.position = clampedPosition;
     }
 }
