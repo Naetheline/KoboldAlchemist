2a80bed5564c62fc17c073e2a5abed6f865173fd
Naethaline <nathalie.megevand@gmail.com>
2019-04-30 11:04:52 +0200

    Added animation for player.

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -7,6 +7,12 @@ public class PlayerControl : MonoBehaviour
     public const int LIMIT = 26;
     public const float ACCELERATION_MOD = 0.5f;
     public const float ACCELERATION_THRESHOLD = 0.1f;
+
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private SpriteRenderer sr;
+
     void Update()
     {
         float acceleration = Input.acceleration.x; 
@@ -19,5 +25,9 @@ public class PlayerControl : MonoBehaviour
         transform.Translate(acceleration * ACCELERATION_MOD, 0, 0);
         Vector3 clampedPosition = new Vector3( Mathf.Clamp(transform.position.x, -LIMIT, LIMIT), transform.position.y, transform.position.z);
         transform.position = clampedPosition;
+
+        sr.flipX = (Mathf.Sign(acceleration) == -1);
+
+        animator.SetFloat("Speed", Mathf.Abs(acceleration));
     }
 }
